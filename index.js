ebd6f85071d4fdeb0a49f4948874168e7f440aaf
Enderman <94362893+StanVliet@users.noreply.github.com>
2022-01-17 10:17:47 +0100

    alle slides next

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+let slideIndex = 0;
 
 function slideshowSetup() {
     document.getElementById("slideshow-next").addEventListener("click", slideshowNext);
+    document.getElementById("slideshow-prev").addEventListener("click", slideshowPrev);
 }
 
 window.addEventListener("load", slideshowSetup);
@@ -8,7 +10,18 @@ window.addEventListener("load", slideshowSetup);
 function slideshowNext() {
     let slides = document.getElementsByClassName("slide");
 
-    slides[0].classList.remove("visible");
-    slides[1].classList.add("visible");
+    slides[slideIndex].classList.remove("visible");
+
+    slideIndex += 1;
+    //slides.length = 5
+    if (slideIndex >= slides.length) {
+        slideIndex = 0;
+    }
+
+    slides[slideIndex].classList.add("visible");
     
+}
+
+function slideshowPrev() {
+
 }
